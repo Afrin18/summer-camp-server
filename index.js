61f115640a2872f8aa5db595f1d7fe422a58c6d8
Afrin18 <gazifayzonnesaafrin18@gmail.com>
2023-06-09 17:30:22 +0600

    insert cls

--- index.js
+++ index.js
@@ -15,30 +15,51 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
 
-    const classesCollection = client.db("summerDB").collection("classes");
-    const instructorsCollection = client.db("summerDB").collection("instructors");
+        const classesCollection = client.db("summerDB").collection("classes");
+        const instructorCollection = client.db("summerDB").collection("instructor");
+        const selectClsCollection = client.db("summerDB").collection("selectcls");
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // class file......
+        app.get('/classes', async (req, res) => {
+            const result = await classesCollection.find().toArray();
+            res.send(result);
+        })
+
+        // instructor file......
+        app.get('/instructor', async (req, res) => {
+            const result = await instructorCollection.find().toArray();
+            res.send(result);
+        })
+
+        // select class collection
+        app.post('/selectcls', async (req, res) => {
+            const item = req.body;
+            const result = await selectClsCollection.insertOne(item);
+            res.send(result);
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
